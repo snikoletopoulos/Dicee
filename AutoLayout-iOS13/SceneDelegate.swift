5295645602b939dc766b981139d8a438531949b1
Stavros <snikoletopoulos@hotmail.com>
2024-04-11 22:17:40 +0300

    Add new code and files

--- AutoLayout-iOS13/SceneDelegate.swift
+++ AutoLayout-iOS13/SceneDelegate.swift
@@ -1,3 +1,11 @@
+//
+//  SceneDelegate.swift
+//  AutoLayout-iOS13
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
