2f7d8909001f2ce44ee369f28d70725cdff882ba
Stavros <snikoletopoulos@hotmail.com>
2024-04-15 01:59:17 +0300

    Format code

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -1,13 +1,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var diceImageViewOne: UIImageView!
-    @IBOutlet var diceImageViewTwo: UIImageView!
+  @IBOutlet var diceImageViewOne: UIImageView!
+  @IBOutlet var diceImageViewTwo: UIImageView!
 
-    @IBAction func rollButtonPressed(_ sender: UIButton) {
-        let dicesImages = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+  @IBAction func rollButtonPressed(_ sender: UIButton) {
+    let dicesImages = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
 
-        diceImageViewOne.image = dicesImages.randomElement()
-        diceImageViewTwo.image = dicesImages.randomElement()
-    }
+    diceImageViewOne.image = dicesImages.randomElement()
+    diceImageViewTwo.image = dicesImages.randomElement()
+  }
 }
